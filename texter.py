8c0d394a2a7a60741a2193493f92aa56be4e0779
Kevin Casey <kacasey@berkeley.edu>
2014-05-04 16:12:37 -0700

    $ hack

--- texter.py
+++ texter.py
@@ -8,7 +8,7 @@ def num_parse(s):
     return "".join([c for c in s if c in string.digits])[-10:]
 def send_message(num, message):
     client.messages.create( 
-    	from_="+1"+num_parse("4084776092"),
+    	from_="+1"+num_parse("7325333935"),
     	to_="+1"+num_parse(num), 
     	body=str(message)
     )
